ef7a6e943c3f7522be99b69687bd63770deed402
somarariana <somarariana@yahoo.com>
2014-02-12 14:52:00 +0100

    start Web Services

---
+++ serverHttp/src/com/h4313/deephouse/serverHttp/webServices/DeepHouseServices.java
@@ -0,0 +1,14 @@
+package com.h4313.deephouse.serverHttp.webServices;
+
+import org.json.JSONObject;
+
+import com.h4313.deephouse.exceptions.DeepHouseException;
+
+public interface DeepHouseServices {
+	
+	public void addSensor(JSONObject json) throws DeepHouseException;
+	
+	public void addActuator(JSONObject json) throws DeepHouseException;
+	
+	public void getHouse() throws DeepHouseException;
+}
