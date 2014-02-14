010d6c5935ae9f6ab0a9c466a7513f4b88a34edd
macbric <macbric>
2014-02-14 10:22:32 +0100

    s

--- serverHttp/src/com/h4313/deephouse/server/main/Main.java
+++ serverHttp/src/com/h4313/deephouse/server/main/Main.java
@@ -28,12 +28,15 @@ public class Main {
 				room.addSensor(DecToHexConverter.decToHex(id++), SensorType.TEMPERATURE);
 				room.addSensor(DecToHexConverter.decToHex(id++), SensorType.WINDOW);
 				room.addSensor(DecToHexConverter.decToHex(id++), SensorType.LIGHT);
+				room.addSensor(DecToHexConverter.decToHex(id++), SensorType.DOOR);
+				room.addSensor(DecToHexConverter.decToHex(id++), SensorType.FLAP);
 				room.addSensor(DecToHexConverter.decToHex(id++), SensorType.PRESENCE);
 
-				room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.LIGHTCONTROL);
-				//room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.AIRCONDITION);
 				room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.RADIATOR);
 				room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.WINDOWCLOSER);
+				room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.LIGHTCONTROL);
+				room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.DOORCONTROL);
+				room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.FLAPCLOSER);
 				
 				// room.connectSensorActuator(sensorId, actuatorId);
 				
