2947dc4042c19821cf903ee5ab86e74f855497a0
somarariana <somarariana@yahoo.com>
2014-02-14 10:58:50 +0100

    webServices: return false if exception

--- serverHttp/src/com/h4313/deephouse/server/webServices/DeepHouseServicesImpl.java
+++ serverHttp/src/com/h4313/deephouse/server/webServices/DeepHouseServicesImpl.java
@@ -9,7 +9,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.core.MediaType;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,8 +16,6 @@ import org.json.JSONObject;
 import com.h4313.deephouse.actuator.ActuatorType;
 import com.h4313.deephouse.dao.HouseDAO;
 import com.h4313.deephouse.exceptions.DeepHouseException;
-import com.h4313.deephouse.exceptions.DeepHouseFormatException;
-import com.h4313.deephouse.exceptions.DeepHouseTypeException;
 import com.h4313.deephouse.housemodel.House;
 import com.h4313.deephouse.housemodel.Room;
 import com.h4313.deephouse.sensor.SensorType;
@@ -30,105 +27,107 @@ import com.h4313.deephouse.util.DeepHouseCalendar;
 @Produces("application/json")
 /*
  * !Important! Defines getters and setters domain class NO methods with no
- * params starting by get... (WebSevice thinks is a getter)
- * Documentation : RESTFul Web Services http://docs.oracle.com/javaee/6/tutorial/doc/gilik.html
+ * params starting by get... (WebSevice thinks is a getter) Documentation :
+ * RESTFul Web Services http://docs.oracle.com/javaee/6/tutorial/doc/gilik.html
  */
 public class DeepHouseServicesImpl implements DeepHouseServices {
 
 	private HouseDAO houseDAO;
-	
-	public DeepHouseServicesImpl() throws DeepHouseException{
+
+	public DeepHouseServicesImpl() throws DeepHouseException {
 		houseDAO = new HouseDAO();
 		House.initInstance(houseDAO);
-		House.getInstance().printInformations();
+
+		House h = House.getInstance();
+		h.printInformations();
+
+		// h.addActuator(0, "Haha1", ActuatorType.DOORCONTROL.name());
+		// h.printInformations();
+
+		// houseDAO.createUpdate(h);
+
 	}
-	
+
 	@GET
 	@Path("/connect")
 	@Override
-	public String connect() throws DeepHouseException {
-		// Initialisation horloge
-		DeepHouseCalendar.getInstance().init();
-		
-		// Initialisation de la maison // TODO : RETIRER POUR LA PRODUCTION
-		try
-		{
-			List<Room> rooms = House.getInstance().getRooms();
-			int id = 0;
-			for(Room room : rooms)
-			{
-				room.addSensor(DecToHexConverter.decToHex(id++), SensorType.TEMPERATURE);
-				room.addSensor(DecToHexConverter.decToHex(id++), SensorType.WINDOW);
-				room.addSensor(DecToHexConverter.decToHex(id++), SensorType.LIGHT);
-				room.addSensor(DecToHexConverter.decToHex(id++), SensorType.PRESENCE);
-
-				room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.LIGHTCONTROL);
-				//room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.AIRCONDITION);
-				room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.RADIATOR);
-				room.addActuator(DecToHexConverter.decToHex(id++), ActuatorType.WINDOWCLOSER);
-				
-				// room.connectSensorActuator(sensorId, actuatorId);
-				
-				room.establishConnections();
-			}
-			System.out.println("Done init");
-			for(Room room : rooms){
-				room.printInformations();
-			}
-		} 
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		
-		String response = null;
+	public String connect() {
 		try {
-			response = new JSONObject().put("success", true).toString();
-		} catch (JSONException e) {
-			throw new DeepHouseFormatException("Json response error : " + e.getMessage());
+			// Initialisation horloge
+			DeepHouseCalendar.getInstance().init();
+
+			// Initialisation de la maison // TODO : RETIRER POUR LA PRODUCTION
+			{
+				List<Room> rooms = House.getInstance().getRooms();
+				int id = 0;
+				for (Room room : rooms) {
+					room.addSensor(DecToHexConverter.decToHex(id++),
+							SensorType.TEMPERATURE);
+					room.addSensor(DecToHexConverter.decToHex(id++),
+							SensorType.WINDOW);
+					room.addSensor(DecToHexConverter.decToHex(id++),
+							SensorType.LIGHT);
+					room.addSensor(DecToHexConverter.decToHex(id++),
+							SensorType.PRESENCE);
+
+					room.addActuator(DecToHexConverter.decToHex(id++),
+							ActuatorType.LIGHTCONTROL);
+					// room.addActuator(DecToHexConverter.decToHex(id++),
+					// ActuatorType.AIRCONDITION);
+					room.addActuator(DecToHexConverter.decToHex(id++),
+							ActuatorType.RADIATOR);
+					room.addActuator(DecToHexConverter.decToHex(id++),
+							ActuatorType.WINDOWCLOSER);
+
+					// room.connectSensorActuator(sensorId, actuatorId);
+
+					room.establishConnections();
+				}
+				System.out.println("Done init");
+				House.getInstance().printInformations();
+			}
+
+			return getSuccessJSONString();
+		} catch (Exception e) {
+			return getErrorJSONString(e);
 		}
-		
-		return response;
 	}
-	
+
 	@Override
 	@POST
 	@Path("/addSensor")
 	@Consumes("application/x-www-form-urlencoded")
-	public String addSensor(@FormParam("piece") String piece, 
+	/* example type = SensorType.DOOR.name(); */
+	public String addSensor(@FormParam("piece") String piece,
 			@FormParam("capteur") String idCapteur,
-			@FormParam("type") String type) throws DeepHouseException {
-		
-		House.getInstance().addSensor(Integer.valueOf(piece), idCapteur, type);
-		
-		String response = null;
+			@FormParam("type") String type) {
+
 		try {
-			response = new JSONObject().put("success", true).toString();
-		} catch (JSONException e) {
-			throw new DeepHouseFormatException("Json response error : " + e.getMessage());
+
+			House.getInstance().addSensor(Integer.valueOf(piece), idCapteur,
+					type);
+			return getSuccessJSONString();
+		} catch (Exception e) {
+			return getErrorJSONString(e);
 		}
-		
-		return response;
 	}
 
 	@Override
 	@POST
 	@Path("/addActuator")
 	@Consumes("application/x-www-form-urlencoded")
-	public String addActuator(@FormParam("piece") String piece, 
+	/* example type = ActuatorType.DOORCONTROL.name(); */
+	public String addActuator(@FormParam("piece") String piece,
 			@FormParam("actionneur") String idActionneur,
-			@FormParam("type") String type) throws DeepHouseException {
-		
-		House.getInstance().addActuator(Integer.valueOf(piece), idActionneur, type);
-		
-		String response = null;
+			@FormParam("type") String type) {
+
 		try {
-			response = new JSONObject().put("success", true).toString();
-		} catch (JSONException e) {
-			throw new DeepHouseFormatException("Json response error : " + e.getMessage());
+			House.getInstance().addActuator(Integer.valueOf(piece),
+					idActionneur, type);
+			return getSuccessJSONString();
+		} catch (Exception e) {
+			return getErrorJSONString(e);
 		}
-		
-		return response;
 	}
 
 	@GET
@@ -142,21 +141,41 @@ public class DeepHouseServicesImpl implements DeepHouseServices {
 	@PUT
 	@Path("/userAction")
 	@Consumes("application/x-www-form-urlencoded")
-	public String userAction(@FormParam("piece") String piece, 
-			@FormParam("typeAction")String typeAction, 
-			@FormParam("valeur") String valeur, 
-			@FormParam("idActionneur") String idActionneur) throws DeepHouseException {
-		
-		House.getInstance().userAction(Integer.valueOf(piece), typeAction, valeur, idActionneur);
-		
-		String response = null;
+	public String userAction(@FormParam("piece") String piece,
+			@FormParam("typeAction") String typeAction,
+			@FormParam("valeur") String valeur,
+			@FormParam("idActionneur") String idActionneur) {
+
 		try {
-			response = new JSONObject().put("success", true).toString();
-		} catch (JSONException e) {
-			throw new DeepHouseFormatException("Json response error : " + e.getMessage());
+			House.getInstance().userAction(Integer.valueOf(piece), typeAction,
+					valeur, idActionneur);
+
+			return getSuccessJSONString();
+		} catch (Exception e) {
+			return getErrorJSONString(e);
 		}
-		
-		return response;
 	}
 
+	private String getErrorJSONString(Exception e) {
+		try {
+			JSONObject json = new JSONObject();
+			json.put("success", false);
+			json.put("msg", e.getMessage());
+			return json.toString();
+		} catch (JSONException e1) {
+			System.out.println(e1.getMessage());
+			return "Json response error : " + e1.getMessage();
+		}
+	}
+
+	private String getSuccessJSONString() {
+		try {
+			JSONObject json = new JSONObject();
+			json.put("success", true);
+			return json.toString();
+		} catch (JSONException e1) {
+			System.out.println(e1.getMessage());
+			return "Json response error : " + e1.getMessage();
+		}
+	}
 }
